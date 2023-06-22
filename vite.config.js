74504f9d3d3ee1f7c656b2f2dc903db1f754ac06
avi <avikhajuria1994@outlook.com>
2023-06-22 20:48:35 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Project_Banking_Website/',
   plugins: [react()],
+  base: '/Project_Banking_Website/',
 })
